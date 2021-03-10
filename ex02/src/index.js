00a95180a521745e49a6b49f6d36124beac515fe
Niloufar Ghesmati <Niloofargst@gmail.com>
2021-03-10 11:57:57 +0100

    added second ex

---
+++ ex02/src/index.js
@@ -0,0 +1,12 @@
+function main() {
+// This is a useless comment in Javascrip
+
+/* document.write("This is another useless comment that serves 
+nothing but merely exists for the sake of grades!");
+*/
+
+}
+
+console.log (main());
+
+module.exports = main;
