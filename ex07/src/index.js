71a429c5fae85a8d37dbf265b9f1cb9bb2513af1
Niloufar Ghesmati <Niloofargst@gmail.com>
2021-03-12 17:53:02 +0100

    redoing

--- ex07/src/index.js
+++ ex07/src/index.js
@@ -1,21 +1,17 @@
 function main() {
-    // Variable declarations 
-    var studyCapVar;
+    var studlyCapVar;
     var properCamelCase;
     var titleCaseOver;
 
-    // Variable assignments
-    studyCapVar = 10;
+    studlyCapVar = 10;
     properCamelCase = "A String";
     titleCaseOver = 9000;
-
+    
     return {
-        studyCapVar,
+        studlyCapVar,
         properCamelCase,
         titleCaseOver
     }
 }
-
-console.log (main());
-
+console.log(main());
 module.exports = main;
